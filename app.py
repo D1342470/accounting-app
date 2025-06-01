defac0f278b30d103fd209f525cbcd319650a639
D1342470 <d1342470@o365.fcu.edu.tw>
2025-06-01 23:11:34 +0800

    Update app.py

--- app.py
+++ app.py
@@ -27,8 +27,8 @@ else:
 if "edit_index" not in st.session_state:
     st.session_state.edit_index = None
 
-st.set_page_config(page_title="簡單記帳", page_icon="📒")
-st.title("📒 簡單記帳 App")
+st.set_page_config(page_title="每日花費記帳&消費分析", page_icon="📒")
+st.title("📒 簡單記帳 ")
 st.markdown("記錄你的每日支出，簡單好用、圖表清晰！")
 
 def save_records():
